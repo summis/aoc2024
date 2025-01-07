452b0dc0a9a5add54ff9fa8d21b3cdd2d31891ee
summis <46009333+summis@users.noreply.github.com>
2025-01-07 21:48:30 +0200

    2/1

--- 01.py
+++ 01.py
@@ -1,13 +1,22 @@
 import sys
+from collections import defaultdict
 
 data = [x.split() for x in sys.stdin.read().splitlines()]
+left = [int(x[0]) for x in data]
+right = [int(x[1]) for x in data]
 
 
 def p1():
-    left = [int(x[0]) for x in data]
-    right = [int(x[1]) for x in data]
-
     return sum(abs(x - y) for x, y in zip(sorted(left), sorted(right)))
 
 
-print(p1())
+def p2():
+    frequency = defaultdict(int)
+
+    for r in right:
+        frequency[r] += 1
+
+    return sum(l * frequency[l] for l in left)
+
+
+print(p1(), p2())
