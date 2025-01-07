afa305aee6c77fddc2cb22d70e50e6862e0c7426
summis <46009333+summis@users.noreply.github.com>
2025-01-07 21:48:30 +0200

    2/6

--- 06.py
+++ 06.py
@@ -1,4 +1,11 @@
+import enum
 import sys
+
+
+class Outcome(enum.Enum):
+    LOOP = enum.auto()
+    OUT = enum.auto()
+
 
 data = {
     col + row * 1j: c
@@ -6,26 +13,48 @@ data = {
     for col, c in enumerate(line)
 }
 
-dir = -1j
-pos = next(k for k, v in data.items() if v == "^")
-visited = set()
+
+def run_simulation(graph, pos, dir):
+    visited = set()
+    visited_turn_points = set()
+
+    while True:
+        visited.add(pos)
+
+        next_pos = pos + dir
+
+        if next_pos not in graph:
+            return Outcome.OUT, visited
+        elif graph[next_pos] == "#":
+            if (pos, dir) in visited_turn_points:
+                return Outcome.LOOP, visited
+
+            visited_turn_points.add((pos, dir))
+            dir *= 1j
+            continue
+        else:
+            pos = next_pos
 
 
-while True:
-    visited.add(pos)
+start_point = next(k for k, v in data.items() if v == "^")
+start_dir = -1j
 
-    next_pos = pos + dir
-
-    if next_pos not in data:
-        break
-    elif data[next_pos] == "#":
-        dir *= 1j
-        continue
-    else:
-        pos = next_pos
-
-
+_, visited = run_simulation(data, start_point, start_dir)
 p1 = len(visited)
 
 
-print(p1)
+def p2():
+    loop_count = 0
+    obstacle_positions = visited - {start_point}
+
+    for point in obstacle_positions:
+        updated_graph = data | {point: "#"}
+        outcome, _ = run_simulation(updated_graph, start_point, start_dir)
+
+        if outcome == Outcome.LOOP:
+            loop_count += 1
+
+    return loop_count
+
+
+print(p1, p2())
