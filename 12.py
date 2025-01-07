a935e8316cdd7331af888b9660b40a90a9786574
summis <46009333+summis@users.noreply.github.com>
2025-01-07 21:48:30 +0200

    2/12

--- 12.py
+++ 12.py
@@ -7,38 +7,77 @@ data = {
 }
 
 
-def partition():
-    all_squares = set(data.keys())
-    areas = []
-
-    while all_squares:
-        todo = {all_squares.pop()}
-        current_area = set()
+def partition_squares_by_label(squares):
+    while squares:
+        todo = {squares.pop()}
+        current = set()
 
         while todo:
             pos = todo.pop()
-            current_area.add(pos)
+            current.add(pos)
 
             for delta in [1, -1, 1j, -1j]:
                 new_pos = pos + delta
 
-                if new_pos in all_squares and data[new_pos] == data[pos]:
+                if new_pos in squares and data[new_pos] == data[pos]:
                     todo.add(new_pos)
-                    all_squares.remove(new_pos)
+                    squares.remove(new_pos)
 
-        areas.append(current_area)
-
-    return areas
+        yield current
 
 
-def calculate_perimeter(squares):
-    return sum(
-        pos + delta not in squares for pos in squares for delta in [1, -1, 1j, -1j]
+def perimeter(squares):
+    return [
+        pos
+        for delta in [1, -1, 1j, -1j]
+        for pos in squares
+        if pos + delta not in squares
+    ]
+
+
+print(
+    sum(
+        len(p) * len(perimeter(p)) for p in partition_squares_by_label(set(data.keys()))
     )
+)
 
 
-def calculate_area(squares):
-    return len(squares)
+def extract_connected_regions(squares):
+    while squares:
+        todo = {squares.pop()}
+        current = set()
+
+        while todo:
+            pos = todo.pop()
+            current.add(pos)
+
+            for delta in [1, -1, 1j, -1j]:
+                new_pos = pos + delta
+
+                if new_pos in squares:
+                    todo.add(new_pos)
+                    squares.remove(new_pos)
+
+        yield current
 
 
-print(sum(calculate_area(x) * calculate_perimeter(x) for x in partition()))
+def calculate_sides(squares):
+    for x in set(pos.real for pos in squares):
+        for direction in [1, -1]:
+            yield from extract_connected_regions(
+                set(z for z in squares if z.real == x and z + direction not in squares)
+            )
+
+    for y in set(pos.imag for pos in squares):
+        for direction in [1j, -1j]:
+            yield from extract_connected_regions(
+                set(z for z in squares if z.imag == y and z + direction not in squares)
+            )
+
+
+print(
+    sum(
+        len(p) * sum(1 for _ in calculate_sides(p))
+        for p in partition_squares_by_label(set(data.keys()))
+    )
+)
