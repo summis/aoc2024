ce3596e0e159043d5325d7a35f9d7df21960a955
summis <46009333+summis@users.noreply.github.com>
2025-01-07 21:48:30 +0200

    1/8

---
+++ 08.py
@@ -0,0 +1,24 @@
+import sys
+
+grid = {
+    col + row * 1j: c
+    for row, line in enumerate(sys.stdin.read().splitlines())
+    for col, c in enumerate(line)
+}
+
+antennas = {marker for marker in grid.values() if marker != "."}
+antenna_positions = {
+    antenna: [k for k, v in grid.items() if v == antenna] for antenna in antennas
+}
+
+antinodes = set()
+for antenna in antennas:
+    positions = antenna_positions[antenna]
+    for x in positions:
+        for y in positions:
+            if x != y:
+                diff = y - x
+                antinodes.add(y + diff)
+                antinodes.add(x - diff)
+
+print(sum(1 for a in antinodes if a in grid))
