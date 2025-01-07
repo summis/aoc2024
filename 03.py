17cdec174337502ab44fe843731a0008001447fe
summis <46009333+summis@users.noreply.github.com>
2025-01-07 21:48:30 +0200

    2/3

--- 03.py
+++ 03.py
@@ -2,11 +2,38 @@ import re
 import sys
 
 data = sys.stdin.read()
-regex = r"mul\((\d+),(\d+)\)"
+
+MULTIPLICATION_REGEX = r"mul\((\d+),(\d+)\)"
 
 
 def p1():
-    return sum(int(x) * int(y) for x, y in re.findall(regex, data))
+    return sum(int(x) * int(y) for x, y in re.findall(MULTIPLICATION_REGEX, data))
 
 
-print(p1())
+CONTROL_REGEX = r"(mul\(\d+,\d+\)|do\(\)|don't\(\))"
+
+
+def filter_enabled_intructions(s):
+    enabled = True
+    ret = ""
+
+    for x in re.findall(CONTROL_REGEX, s):
+        if x == "do()":
+            enabled = True
+        elif x == "don't()":
+            enabled = False
+        else:
+            if enabled:
+                ret += x
+
+    return ret
+
+
+def p2():
+    return sum(
+        int(x) * int(y)
+        for x, y in re.findall(MULTIPLICATION_REGEX, filter_enabled_intructions(data))
+    )
+
+
+print(p1(), p2())
